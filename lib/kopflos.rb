fde22b53cc553682f068ec2b5f2da9f284f64829
Niklas Hofer <niklas+dev@lanpartei.de>
2011-05-09 08:31:21 +0200

    Kopflos.start is a shortcut to start Xvfb

--- lib/kopflos.rb
+++ lib/kopflos.rb
@@ -1,1 +1,9 @@
 require 'kopflos/xvfb'
+
+module Kopflos
+  def self.start(opts={})
+    @server = Xvfb.new(opts)
+    @server.start
+    @server
+  end
+end
--- spec/kopflos_spec.rb
+++ spec/kopflos_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 describe "Kopflos" do
+  describe "starting without block" do
+    it "should default to Xvfb" do
+      xvfb = mock("Xvfb")
+      xvfb.should_receive(:start).and_return(true)
+      Kopflos::Xvfb.should_receive(:new).and_return(xvfb)
+      Kopflos.start
+    end
+  end
 end
