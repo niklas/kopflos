d8cb54ee0c58c135cecd8a13c8e2d98d0871fa89
Niklas Hofer <niklas+dev@lanpartei.de>
2015-03-18 00:29:58 +0100

    let cucumber fail if at least one scenario fails
    Without this, even failing scenarios do not cause cucumber to exit with
    code != 0, this showing false green builds on CI servers.
    
    Because we hook into at_exit, the real exit code from cucumber is
    swallowed. This only began happing with ruby 1.9. To mitigate that, we
    deal with failed scenarios ourselves and provide t a failing error code
    in the right case.
    
    Caveat: this might still swallow custom errors codes from cucumber.
    
    See also:
    "at_exit is not an AfterAll alternative"
    https://github.com/cucumber/cucumber/issues/441
    
    "Failing Scenarios Exiting with 0"
    https://github.com/cucumber/cucumber/issues/635
    
    Bug caused by similar error
    "Make failing cucumber fail CI #880"
     https://github.com/moneyadviceservice/frontend/pull/880

--- lib/kopflos/cucumber.rb
+++ lib/kopflos/cucumber.rb
@@ -1,9 +1,16 @@
 require 'kopflos'
 
+@exit_code = 0
+
 Before '@javascript' do |scenario|
   Kopflos.start :reuse => true
 end
 
+After do |scenario|
+  @exit_code = 1 if scenario.failed?
+end
+
 at_exit do
   Kopflos.stop
+  exit(@exit_code)
 end
