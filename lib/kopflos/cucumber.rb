6e0cd4786c19a2e48a30978ac2a19f63d45cb234
Niklas Hofer <niklas+dev@lanpartei.de>
2015-03-18 00:56:49 +0100

    ^ did not fix it. trying a global var

--- lib/kopflos/cucumber.rb
+++ lib/kopflos/cucumber.rb
@@ -1,16 +1,16 @@
 require 'kopflos'
 
-@exit_code = 0
+$kopflos_exit_code = 0
 
 Before '@javascript' do |scenario|
   Kopflos.start :reuse => true
 end
 
 After do |scenario|
-  @exit_code = 1 if scenario.failed?
+  $kopflos_exit_code = 1 if scenario.failed?
 end
 
 at_exit do
   Kopflos.stop
-  exit(@exit_code)
+  exit($kopflos_exit_code)
 end
